7a00ab9565a836440081e730057ec25c49b84c82
Miquel Avellaneda <mavellanedal2223@politecnics.barcelona>
2026-01-26 19:42:43 +0100

    Add TypeORM configuration and flag --disable-host-check to frontend compilation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false,
+      migrationsRun: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
